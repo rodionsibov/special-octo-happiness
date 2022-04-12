95200637a1b6bc8ff3078eaea9edb6f8376b236c
Rodion Sibov <rodionsibov@gmail.com>
2022-04-12 15:51:40 +0300

    first commit

---
+++ src/app/todos/components/todo/todo.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from '../../types/todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  @Input('todo') todoProps!: Todo
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
